7d1ad189e33bf09c6dd73d2ff30396014e4ba419
mosquito <wenwen.zhang@yoho.cn>
2017-07-11 19:33:59 +0800

    add podfile

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, ‘8.0’
+use_frameworks!
+
+target ‘Reader’ do
+ 
+    pod 'SnapKit', '~> 3.2.0'
+    pod 'PDFReader', '~> 2.4.0'
+    pod 'KGFloatingDrawer', '~> 0.2.0'
+
+end
---
+++ Reader-Bridging_header.h
@@ -0,0 +1,13 @@
+//
+//  Reader-Bridging_header.h
+//  Reader
+//
+//  Created by mosquito on 2017/7/11.
+//  Copyright © 2017年 mosquito. All rights reserved.
+//
+
+#ifndef Reader_Bridging_header_h
+#define Reader_Bridging_header_h
+
+
+#endif /* Reader_Bridging_header_h */
